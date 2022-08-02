2f17c479d956f2a220c2611185ef3ba44021c22b
Luis <kira.lc2@gmail.com>
2022-08-02 14:46:09 -0500

    ok

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -16,9 +16,95 @@ class Citas{
         this.citas = [];
     }
 
+    agragarCita(cita){
+        this.citas = [...this.citas, cita];
+        console.log(this.citas);
+    }
+
 }
 
 class UI{
+
+    imprimirAlerta(mensaje, tipo){
+        // crear el div
+        const divmensaje = document.createElement("div");
+        divmensaje.classList.add("text-center", "alert", "d-block", "col-12");
+
+        //agregar clase al ripo de error 
+
+        if(tipo === "error"){
+            divmensaje.classList.add("alert-danger");
+        }else{
+            divmensaje.classList.add("alert-success");
+        }
+
+        //div
+        divmensaje.textContent = mensaje;
+
+        document.querySelector("#contenido").insertBefore(divmensaje, document.querySelector(".agregar-cita"));
+
+        setTimeout(() => {
+            divmensaje.remove();
+        }, 3000);
+    }
+
+    imprimirCitas({citas}){
+
+        this.limpiarHtml();
+        
+        citas.forEach(cita => {
+            const { mascota, propietario, fecha, hora, sintomas, id, telefono} = cita
+
+            const divCita =document.createElement("div");
+            divCita.classList.add("cita", 'p-3');
+            divCita.dataset.id = id;
+
+            const mascotaparrafo = document.createElement("h2");
+            mascotaparrafo.classList.add("card-title", "font-weight-bolder");
+            mascotaparrafo.textContent = mascota;
+
+            const propietarioParrafo = document.createElement("p");
+            propietarioParrafo.innerHTML = `
+            <span class="font-weight-bolder">Propietario: </span> ${propietario}
+            `;
+
+            const telefonoParrafo = document.createElement("p");
+            telefonoParrafo.innerHTML = `
+            <span class="font-weight-bolder">telefono: </span> ${telefono}
+            `;
+
+            const fechaParrafo = document.createElement("p");
+            fechaParrafo.innerHTML = `
+            <span class="font-weight-bolder">fecha: </span> ${fecha}
+            `;
+
+            const horaParrafo = document.createElement("p");
+            horaParrafo.innerHTML = `
+            <span class="font-weight-bolder">hora: </span> ${hora}
+            `;
+
+            const sintomasParrafo = document.createElement("p");
+            sintomasParrafo.innerHTML = `
+            <span class="font-weight-bolder">sintomas: </span> ${sintomas}
+            `;
+            
+
+            divCita.appendChild(mascotaparrafo);
+            divCita.appendChild(propietarioParrafo);
+            divCita.appendChild(telefonoParrafo);
+            divCita.appendChild(fechaParrafo);
+            divCita.appendChild(horaParrafo);
+            divCita.appendChild(sintomasParrafo);
+
+            contenedor_citas.appendChild(divCita);
+        });
+    }
+
+    limpiarHtml(){
+        while (contenedor_citas.firstChild) {
+            contenedor_citas.removeChild(contenedor_citas.firstChild);
+        }
+    }
 
 }
 
@@ -54,7 +140,6 @@ const citaObj = {
 //agregar datos al objeto de lista
 function datosCita(e){
    citaObj[ e.target.name] = e.target.value;
-   console.log(citaObj)
 }
 
 // valida y agrega una nueva cita a la clase de citas
@@ -66,8 +151,28 @@ function nuevaCita(e){
     const { mascota, propietario, fecha, hora, sintomas} = citaObj
 
     if(mascota === "" || propietario === "" || fecha === "" || hora === "" || sintomas === ""){
-        console.log("todos los campos son obligatorios");
+        ui.imprimirAlerta("Todos los Campos son Obligatorios", "error");
         return;
     }
 
+    //generar un ID unico
+    citaObj.id = Date.now();
+
+    administrarCitas.agragarCita({...citaObj});
+
+    reiniciarObjeto();
+
+    formulario.reset();
+
+    ui.imprimirCitas(administrarCitas);
+
+}
+
+function reiniciarObjeto(){
+    citaObj.mascota= "";
+    citaObj.propietario = "";
+    citaObj.telefono = "";
+    citaObj.fecha = "";
+    citaObj.hora = "";
+    citaObj.sintomas = "";    
 }
