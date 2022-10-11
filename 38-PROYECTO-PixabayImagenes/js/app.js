d88594448f15f260e7c1af693b1e114cadfcfeae
Luis <kira.lc2@gmail.com>
2022-10-10 19:17:56 -0500

    proyecto terminado paginacion

--- 38-PROYECTO-PixabayImagenes/js/app.js
+++ 38-PROYECTO-PixabayImagenes/js/app.js
@@ -1,5 +1,10 @@
 const formulario = document.querySelector('#formulario');
 const resultado = document.querySelector('#resultado');
+const paginacion = document.querySelector('#paginacion');
+
+const registroPorPagina = 40;
+let totalPaginas,iterador;
+let paginaActual = 1
 
 window.onload = () =>{
     formulario.addEventListener('submit', validarFormulario);
@@ -15,7 +20,7 @@ function validarFormulario(e){
         return;
     }
 
-    buscarimagenes(termino)
+    buscarimagenes()
 }
 
 function mandarMensaje(mensaje){
@@ -41,13 +46,92 @@ function mandarMensaje(mensaje){
 }
 
 
-function buscarimagenes(termino){
+function buscarimagenes(){
+
+    const termino = document.querySelector('#termino').value;
+
     const key = '30415895-2ffa4b509f9a1cfeaaeb0bae7';
-    const url = `https://pixabay.com/api/?key=${key}&q=${termino}`;
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registroPorPagina}&page=${paginaActual}`;
 
     console.log(url);
 
     fetch(url)
     .then(respuesta => respuesta.json())
-    .then(resultado => console.log(resultado))
+    .then(resultado =>{ 
+        totalPaginas = calcularPAginas(resultado.totalHits);
+        mostrarImagenes(resultado.hits)})
+}
+
+
+//GENERADOR DE PAGINAS
+
+function *crearPaginador(total){
+    for (let i = 1; i <= total; i++){
+        yield i;
+    }
+}
+
+
+function calcularPAginas(total){
+    return parseInt(Math.ceil(total/registroPorPagina));
+}
+
+function mostrarImagenes(imagenes){
+   while (resultado.firstChild){
+        resultado.removeChild(resultado.firstChild)
+    }
+
+    //iterar sobre las imagenes 
+
+    imagenes.forEach(imagen => {
+        const {previewURL, likes, views, largeImageURL} = imagen;      
+        
+        resultado.innerHTML += `
+        <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+            <div class="bg-white">
+                <img class="w-full" src="${previewURL}">
+
+                <div class="p-4">
+                    <p class="font-bold"> ${likes} <span class="font-light">Me Gusta</span></p>
+                    <p class="font-bold"> ${views} <span class="font-light">Veces Vista</span></p>
+                    <a class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1"
+                        href= ${largeImageURL}" target="_black" rel="noopener noreferrer">
+                    Ver Imagen
+                    </a>
+                </div>
+            </div>
+        </div>
+        `;
+    });
+
+    while(paginacion.firstChild){
+        paginacion.removeChild(paginacion.firstChild)
+    }
+
+     imprimirPaginador();
+}
+
+function imprimirPaginador(){
+    iterador = crearPaginador(totalPaginas);
+
+    while(true){
+        const {value, done} = iterador.next()       
+
+        if (done) return;
+
+        const boton = document.createElement('a');
+        boton.href = '#'
+        boton.dataset.pagina = value
+        boton.textContent = value
+        boton.classList.add('siguiente', 'bg-yellow-400', 'px-4', 'py-1', 'mr-2', 'font-bold', 'mb-4', 'uppercase', 'rounded' );
+
+        boton.onclick = () =>{
+            paginaActual  =value
+            buscarimagenes()
+        }
+
+
+
+        paginacion.appendChild(boton);
+    }
 }
