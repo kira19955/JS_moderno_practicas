c65b4ea6b847e8d678c60aae4fe1ecb9f5ca85c2
luis antonio <kira.lc2@gmail.com>
2023-02-28 23:39:31 -0600

    ok

--- 44-PROYECTO-CRM-CRUD-REST/js/API.js
+++ 44-PROYECTO-CRM-CRUD-REST/js/API.js
@@ -1,5 +1,7 @@
 const url = 'http://localhost:4000/clientes';
 
+
+//CUANDO SE CREA UN NUEVO CLIENTE
 export const nuevoCliente = async cliente =>{
     try {
         //para mandar a post se agrega la configuracion
@@ -12,7 +14,34 @@ export const nuevoCliente = async cliente =>{
         });
 
         window.location.href = 'index.html';
-        
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//OBTIENE LOS CLIENTES
+
+export const obtenerClientes = async () =>{
+    try {
+        const resultado = await fetch(url)
+        const clientes = await resultado.json()
+
+        return clientes
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+//elimina un cliente
+
+export const eliminarCliente =async (id) =>{
+    try {
+        await fetch(`${url}/${id}`,{
+            method: 'DELETE',
+
+        })
     } catch (error) {
         console.log(error)
     }
